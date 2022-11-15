24ce815ebae1ef868118276f84805f84b5e4d0e7
sami-brassier <115148513+sami-brassier@users.noreply.github.com>
2022-11-15 11:46:47 +0100

    Update commands-gits.sh
    ajout du pull

--- commands-gits.sh
+++ commands-gits.sh
@@ -2,3 +2,4 @@ git clone git@github.com:nomuser/repo
 git push remotename remote-branchname
 git push -u remotename remote-branchname
 git push remotename local-branchname:remote-branchname
+git pull remotename
