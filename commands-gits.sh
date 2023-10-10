3574e800b9588d6758bdb55499411d34b928bc01
sami-brassier <115148513+sami-brassier@users.noreply.github.com>
2023-10-10 10:38:17 +0200

    Delete commands-gits.sh
    ancien

--- commands-gits.sh
+++
@@ -1,5 +0,0 @@
-git clone git@github.com:nomuser/repo
-git push remotename remote-branchname
-git push -u remotename remote-branchname
-git push remotename local-branchname:remote-branchname
-git pull remotename
