cd07a59766adcc225a7242aa3fcc3fab669f505c
sami-brassier <115148513+sami-brassier@users.noreply.github.com>
2023-10-10 10:38:54 +0200

    Add files via upload
    reajout du fichier apres oubli de switch

---
+++ commands-gits.sh
@@ -0,0 +1,13 @@
+git clone git@github.com:nomuser/repo
+
+git add --all
+git commit -m "commentaire"
+
+git push remotename remote-branchname
+git push -u remotename remote-branchname
+git push remotename local-branchname:remote-branchname
+
+git pull remotename
+
+git branch branchnom
+git switch branchnom
